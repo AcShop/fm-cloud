02f73d8d23e5f11d66993fac64b49e38661dd5ea
王爵 <biezhi.me@gmail.com>
2014-09-19 18:01:48 +0800

    完成播放音乐功能

--- src/main/webapp/static/js/front/music.js
+++ src/main/webapp/static/js/front/music.js
@@ -7,32 +7,35 @@ var cssSelector = {
 };
 
 var options = {
+//	playlistOptions: {
+//	    autoPlay: true
+//	},
 	swfPath : $unique.cdn + "/static/js/jq/Jplayer.swf",
 	supplied : "ogv, m4v, oga, mp3"
 };
 var myPlaylist = null;
 
 $(function() {
-//	var playlist = [{
-//		title : "Hidden",
-//		artist : "Miaow",
-//		mp3 : "http://jq22.qiniudn.com/i1.mp3",
-//		//oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
-//		poster : "http://33.media.tumblr.com/0b35eb42176eedbf4a96e52efa760875/tumblr_mxp7a0v3fr1rqx86wo1_500.png"
-//	}, {
-//		title : "Cro Magnon Man",
-//		artist : "The Stark Palace",
-//		mp3 : "http://jq22.qiniudn.com/i2.mp3",
-//		//oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
-//		poster : "http://33.media.tumblr.com/bf9dc125a47dcca91ce5b3575bc3ba92/tumblr_nbmb3j8nU51sq3g2zo1_500.png"
-//	}, {
-//		title : "Bubble",
-//		m4a : "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-//		oga : "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg",
-//		poster : "http://31.media.tumblr.com/810b1125a8b9e9f192d009ef58dceb07/tumblr_nbe8wsmKuz1rknpqyo1_500.jpg"
-//	}];
-//
-//	myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
+	var playlist = [{
+		title : "Hidden",
+		artist : "Miaow",
+		mp3 : "http://jq22.qiniudn.com/i1.mp3",
+		//oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+		poster : $unique.cdn + "/static/img/tumblr_mxp7a0v3fr1rqx86wo1_500.png"
+	}, {
+		title : "Cro Magnon Man",
+		artist : "The Stark Palace",
+		mp3 : "http://jq22.qiniudn.com/i2.mp3",
+		//oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+		poster : $unique.cdn + "/static/img/tumblr_nbmb3j8nU51sq3g2zo1_500.png"
+	}, {
+		title : "Bubble",
+		m4a : "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+		oga : "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg",
+		poster : $unique.cdn + "/static/img/tumblr_nbe8wsmKuz1rknpqyo1_500.jpg"
+	}];
+
+	myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
 });
 
 /**
@@ -48,17 +51,15 @@ $unique.music.playerMp3 = function(mid) {
 		};
 		$.get(url, param, function(data) {
 			if (data) {
-				var playlist = [{
-					title : data.song,
-					artist : data.singer,
-					mp3 : data.mp3_url,
-					ogg : data.mp3_url,
-					poster : data.cover_url
-				}];
-				myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
-				// 播放
-				$("#jquery_jplayer").jPlayer("playHeadTime", 0);
-				//$unique.music.loadAudio(data.mp3_url, data.song);
+				myPlaylist.setPlaylist([{
+	                title: data.song,
+	                artist: data.singer,
+	                mp3: data.mp3_url,
+	                poster: data.cover_url
+	            }]);
+				//设置自动播放
+				myPlaylist.option("autoPlay", true);
+				myPlaylist.play(0);
 			}
 		}, 'json');
 	}
